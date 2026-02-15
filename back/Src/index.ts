c8d3d57eede7a70bfb3a26d07f499693005c8d7b
Gameinwza <apunbunchu@gmail.com>
2026-02-15 20:44:24 +0700

    fix port

--- back/Src/index.ts
+++ back/Src/index.ts
@@ -4,10 +4,16 @@ import { paymentRoutes } from "./routes/payment.route";
 import { vehicleRoutes } from "./routes/vehicle.route";
 import { adminRoutes } from "./routes/admin.route";
 import { authRoutes } from "./routes/auth.route";
-import cors from "@elysiajs/cors";
 import { userRoutes } from "./routes/user.route";
+import cors from "@elysiajs/cors";
 
 const app = new Elysia();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*",
+  })
+);
 
 app.use(reservationRoutes);
 app.use(paymentRoutes);
@@ -16,13 +22,9 @@ app.use(adminRoutes);
 app.use(authRoutes);
 app.use(userRoutes);
 
-// app.get("/ping", () => "pong");
+// ✅ ใช้ PORT จาก Render
+const port = Number(process.env.PORT) || 3000;
 
+app.listen(port);
 
-app.use(
-    cors({
-        origin: process.env.FRONTEND_URL,
-    })
-)
-app.listen(3000);
-console.log("Server running on http://localhost:3000");
+console.log(`🚀 Server running on port ${port}`);
